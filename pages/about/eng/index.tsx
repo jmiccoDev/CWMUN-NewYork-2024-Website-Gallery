64030d6533ec3d3c9743fd04eb382533bcb3c877
mirkou <fiorinimirko2006@gmail.com>
2024-06-06 10:49:27 +0200

    added div alert

--- pages/about/eng/index.tsx
+++ pages/about/eng/index.tsx
@@ -1,17 +1,17 @@
-// pages/about.js
 import Image from 'next/image';
 import Link from 'next/link';
+import { XMarkIcon } from '@heroicons/react/20/solid'
 
 export default function Eng() {
   return (
     <div className="bg-gradient-to-br from-orange-400 via-red-500 to-yellow-300 min-h-screen">
-      <header className="bg-gradient-to-r from-orange-700 via-orange-600 to-orange-700 text-white">
+      <header className="bg-gradient-to-r from-orange-700 via-orange-600 to-orange-700 text-white relative">
         <div className="container mx-auto flex justify-between items-center py-4 px-4">
           <div className="flex items-center flex-1">
-            <Image 
-              src="/Logo CWMUN.png" 
-              alt="Logo" 
-              width={100} 
+            <Image
+              src="/Logo CWMUN.png"
+              alt="Logo"
+              width={100}
               height={100}
               className='cursor-pointer'
               style={{ filter: 'brightness(0) invert(1)' }}
@@ -34,59 +34,80 @@ export default function Eng() {
         </div>
       </header>
 
+      <div className="relative isolate flex items-center gap-x-6 overflow-hidden rounded-lg px-4 py-3 sm:px-3 sm:before:flex-1" style={{ background: 'linear-gradient(to right, #FFA500, #FF6347)' }}>
+        <div className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl" aria-hidden="true"></div>
+        <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+          <p className="text-sm leading-6 text-white">
+            <strong className="font-semibold">Newspaper News!</strong>
+            <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current text-orange-200" aria-hidden="true">
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            The project I was involved in has been published in the Gazzetta del Sud.
+          </p>
+          <a href="https://gazzettadelsud.it/?p=1879677" target="_blank" className="flex-none rounded-full bg-red-600 px-3.5 py-2 text-sm font-semibold text-white shadow-md transition hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
+            Visit! <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+            <span className="sr-only">Dismiss</span>
+            <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
+          </button>
+        </div>
+      </div>
+
+
       <main className="container mx-auto px-4 py-8 bg-gray-800 bg-opacity-75 text-white mt-8 rounded-lg shadow-lg mb-16">
+
         <h1 className="text-4xl font-bold mb-8">About It</h1>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Introduzione</h2>
+          <h2 className="text-2xl font-semibold mb-2">Introduction</h2>
           <p>
-            Io e altri ragazzi del Verona Trento abbiamo deciso di candidarci alla borsa di studio che l’Associazione Diplomatici offre; questa borsa di studio permette ai selezionati, scelti tramite colloquio, di partecipare al Change the World Model United Nations (CWMUN).
+            Myself and other students from Verona Trento decided to apply for the scholarship offered by the Diplomatic Association; this scholarship allows selected candidates, chosen through an interview, to participate in the Change the World Model United Nations (CWMUN).
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Cos'è il CWMUN</h2>
+          <h2 className="text-2xl font-semibold mb-2">What is CWMUN</h2>
           <p>
-            Il CWMUN è un programma internazionale a cui partecipano ogni anno più di 10.000 studenti di tutto il mondo desiderosi di dibattere i principali temi dell'agenda politica internazionale. Il CWMUN è il più grande Forum giovanile internazionale del pianeta, che attualmente si svolge a New York City, Abu Dhabi, Dubai, Parigi, Singapore e Roma. L'esperienza è pensata per i giovani che vogliono cambiare il mondo e migliorare se stessi.
+            CWMUN is an international program attended by over 10,000 students worldwide each year eager to discuss the main topics of the international political agenda. CWMUN is the largest international youth forum on the planet, currently held in New York City, Abu Dhabi, Dubai, Paris, Singapore, and Rome. The experience is designed for young people who want to change the world and improve themselves.
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">La nostra Destinazione</h2>
+          <h2 className="text-2xl font-semibold mb-2">Our Destination</h2>
           <p>
-            Noi abbiamo scelto New York come destinzione in quanto la migliore e la piú suggestiva ma anche la piú numerosa per quanto riguarda i partecipanti per vivere la miglior esperienza possibile. Siamo stati assegnati, a gruppi di due,alle varie delegazioni disponibili distribuite nelle commissioni dell’onu ognuna riguardante un argomento diverso (UNESCO, ECOSOC, UNOOSA,ecc…).
+            We chose New York as our destination because it is the best and most suggestive but also the most numerous in terms of participants to experience the best possible experience. We have been assigned, in groups of two, to various available delegations distributed in the UN commissions, each dealing with a different topic (UNESCO, ECOSOC, UNOOSA, etc.).
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Obiettivo della Conferenza</h2>
+          <h2 className="text-2xl font-semibold mb-2">Conference Objective</h2>
           <p>
-            Lo scopo della conferenza é stringere collaborazioni con altri paesi per creare una grande commissione per creare il working paper cioé il foglio dove si scrivono le principali problematiche e quelle riscontrate durante la conferenza e le loro eventuali soluzioni.
-            Le collaborazioni ovviamente devono avere una coerenza diplomatica, cioé che segue le ricorrenti relazioni tra stati (ad esempio russia e stati uniti non potrebbero collaborare in quanto non condividono le stesse idee).
+            The purpose of the conference is to build collaborations with other countries to create a large commission to create the working paper, which is the document where the main issues and those encountered during the conference and their possible solutions are written. The collaborations must obviously have diplomatic coherence, i.e., they must follow the recurring relations between states (for example, Russia and the United States could not collaborate as they do not share the same ideas).
           </p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">Benefici dell'Esperienza</h2>
+          <h2 className="text-2xl font-semibold mb-2">Experience Benefits</h2>
           <p>
-            Questa esperienza ti forma, non solo nel parlare inglese, ma anche nel relazionarsi con le altre persone sia in modo diplomatico dentro la conferenza che in modo informale fuori con persone dalle svariate culture.
+            This experience shapes you, not only in speaking English but also in relating to other people both diplomatically within the conference and informally outside with people from various cultures.
           </p>
         </section>
       </main>
-   {/* Simple footer (Made by Mirko Fiorini. All rights are reserved.) */}
-   <footer className="bg-gradient-to-r text-white py-4 text-center">
-  <p>Website created with love by{" "}
-    <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Mirko Fiorini</a>{" "}
-    and{" "}
-    <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Sergio Serraino</a>.
-    <br />
-    Special thanks to the{" "}
-    <a href="https://www.diplomatici.it/" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Associazione Diplomatici</a>.
-    <br />
-    All rights reserved.
-  </p>
-</footer>
+      <footer className="bg-gradient-to-r text-white py-4 text-center">
+        <p>Website created with love by{" "}
+          <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Mirko Fiorini</a>{" "}
+          and{" "}
+          <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Sergio Serraino</a>.
+          <br />
+          Special thanks to the{" "}
+          <a href="https://www.diplomatici.it/" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Associazione Diplomatici</a>.
+          <br />
+          All rights reserved.
+        </p>
+      </footer>
     </div>
-    
   );
 }
--- pages/about/it/index.tsx
+++ pages/about/it/index.tsx
@@ -1,17 +1,17 @@
-// pages/about.js
 import Image from 'next/image';
 import Link from 'next/link';
+import { XMarkIcon } from '@heroicons/react/20/solid'
 
-export default function It() {
+export default function Eng() {
   return (
     <div className="bg-gradient-to-br from-orange-400 via-red-500 to-yellow-300 min-h-screen">
-      <header className="bg-gradient-to-r from-orange-700 via-orange-600 to-orange-700 text-white">
+      <header className="bg-gradient-to-r from-orange-700 via-orange-600 to-orange-700 text-white relative">
         <div className="container mx-auto flex justify-between items-center py-4 px-4">
           <div className="flex items-center flex-1">
-            <Image 
-              src="/Logo CWMUN.png" 
-              alt="Logo" 
-              width={100} 
+            <Image
+              src="/Logo CWMUN.png"
+              alt="Logo"
+              width={100}
               height={100}
               className='cursor-pointer'
               style={{ filter: 'brightness(0) invert(1)' }}
@@ -28,64 +28,87 @@ export default function It() {
               <a className="hover:bg-white hover:text-orange-600 px-3 py-2 rounded transition duration-300">About</a>
             </Link>
             <Link legacyBehavior href="/about/eng">
-              <a className="hover:bg-white hover:text-orange-600 px-3 py-2 rounded transition duration-300">Translate 🇬🇧</a>
+              <a className="hover:bg-white hover:text-orange-600 px-3 py-2 rounded transition duration-300">Traduci 🇬🇧</a>
             </Link>
           </nav>
         </div>
       </header>
 
+      <div className="relative isolate flex items-center gap-x-6 overflow-hidden rounded-lg px-4 py-3 sm:px-3 sm:before:flex-1" style={{ background: 'linear-gradient(to right, #FFA500, #FF6347)' }}>
+        <div className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl" aria-hidden="true"></div>
+        <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+          <p className="text-sm leading-6 text-white">
+            <strong className="font-semibold">Notizia sul giornale!</strong>
+            <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current text-orange-200" aria-hidden="true">
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            Il progetto a cui sono stato partecipe è stato pubblicato sulla Gazzetta del Sud.
+          </p>
+          <a href="https://gazzettadelsud.it/?p=1879677" target="_blank" className="flex-none rounded-full bg-red-600 px-3.5 py-2 text-sm font-semibold text-white shadow-md transition hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
+            Visita! <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+            <span className="sr-only">Dismiss</span>
+            <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
+          </button>
+        </div>
+      </div>
+
       <main className="container mx-auto px-4 py-8 bg-gray-800 bg-opacity-75 text-white mt-8 rounded-lg shadow-lg mb-16">
+
         <h1 className="text-4xl font-bold mb-8">About It</h1>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Introduction</h2>
+          <h2 className="text-2xl font-semibold mb-2">Introduzione</h2>
           <p>
-            I and other students from Verona Trento decided to apply for the scholarship offered by the Associazione Diplomatici; this scholarship allows selected students, chosen through an interview, to participate in the Change the World Model United Nations (CWMUN).
+            Io e altri ragazzi del Verona Trento abbiamo deciso di candidarci alla borsa di studio che l’Associazione Diplomatici offre; questa borsa di studio permette ai selezionati, scelti tramite colloquio, di partecipare al Change the World Model United Nations (CWMUN).
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">What is CWMUN</h2>
+          <h2 className="text-2xl font-semibold mb-2">Cos'è il CWMUN</h2>
           <p>
-            CWMUN is an international program attended each year by over 10,000 students from around the world eager to debate the main issues on the international political agenda. CWMUN is the largest international youth forum on the planet, currently held in New York City, Abu Dhabi, Dubai, Paris, Singapore, and Rome. The experience is designed for young people who want to change the world and improve themselves.
+            Il CWMUN è un programma internazionale a cui partecipano ogni anno più di 10.000 studenti di tutto il mondo desiderosi di dibattere i principali temi dell'agenda politica internazionale. Il CWMUN è il più grande Forum giovanile internazionale del pianeta, che attualmente si svolge a New York City, Abu Dhabi, Dubai, Parigi, Singapore e Roma. L'esperienza è pensata per i giovani che vogliono cambiare il mondo e migliorare se stessi.
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Our Destination</h2>
+          <h2 className="text-2xl font-semibold mb-2">La nostra Destinazione</h2>
           <p>
-            We chose New York as the best and most exciting destination, also the most numerous in terms of participants, to have the best possible experience. We were assigned, in groups of two, to the various available delegations distributed in the UN commissions each dealing with a different topic (UNESCO, ECOSOC, UNOOSA, etc...).
+            Noi abbiamo scelto New York come destinzione in quanto la migliore e la piú suggestiva ma anche la piú numerosa per quanto riguarda i partecipanti per vivere la miglior esperienza possibile. Siamo stati assegnati, a gruppi di due,alle varie delegazioni disponibili distribuite nelle commissioni dell’onu ognuna riguardante un argomento diverso (UNESCO, ECOSOC, UNOOSA,ecc…).
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Conference Goal</h2>
+          <h2 className="text-2xl font-semibold mb-2">Obiettivo della Conferenza</h2>
           <p>
-            The goal of the conference is to forge collaborations with other countries to create a large commission to draft the working paper, which is the document where the main issues and those encountered during the conference and their possible solutions are written. Collaborations obviously need to have diplomatic coherence, meaning they follow the recurring relations between states (e.g., Russia and the United States could not collaborate as they do not share the same ideas).
+            Lo scopo della conferenza é stringere collaborazioni con altri paesi per creare una grande commissione per creare il working paper cioé il foglio dove si scrivono le principali problematiche e quelle riscontrate durante la conferenza e le loro eventuali soluzioni.
+            Le collaborazioni ovviamente devono avere una coerenza diplomatica, cioé che segue le ricorrenti relazioni tra stati (ad esempio russia e stati uniti non potrebbero collaborare in quanto non condividono le stesse idee).
           </p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">Benefits of the Experience</h2>
+          <h2 className="text-2xl font-semibold mb-2">Benefici dell'Esperienza</h2>
           <p>
-            This experience shapes you, not only in speaking English but also in relating to other people both diplomatically within the conference and informally outside with people from various cultures.
+            Questa esperienza ti forma, non solo nel parlare inglese, ma anche nel relazionarsi con le altre persone sia in modo diplomatic
+            o dentro la conferenza che in modo informale fuori con persone dalle svariate culture.
           </p>
-        </section>+
+        </section>
       </main>
-   {/* Simple footer (Made by Mirko Fiorini. All rights are reserved.) */}
-   <footer className="bg-gradient-to-r text-white py-4 text-center">
-  <p>Website created with love by{" "}
-    <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Mirko Fiorini</a>{" "}
-    and{" "}
-    <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Sergio Serraino</a>.
-    <br />
-    Special thanks to the{" "}
-    <a href="https://www.diplomatici.it/" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Associazione Diplomatici</a>.
-    <br />
-    All rights reserved.
-  </p>
-</footer>
+      <footer className="bg-gradient-to-r text-white py-4 text-center">
+        <p>Website created with love by{" "}
+          <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Mirko Fiorini</a>{" "}
+          and{" "}
+          <a href="https://github.com/jmiccoDev" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Sergio Serraino</a>.
+          <br />
+          Special thanks to the{" "}
+          <a href="https://www.diplomatici.it/" target="_blank" className="font-semibold hover:text-white" rel="noreferrer">Associazione Diplomatici</a>.
+          <br />
+          All rights reserved.
+        </p>
+      </footer>
     </div>
-    
   );
 }
