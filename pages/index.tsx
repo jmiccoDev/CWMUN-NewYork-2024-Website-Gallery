611af628b9331ea5bb5e5147dac05bdd8ea27940
mirkou <fiorinimirko2006@gmail.com>
2024-06-06 08:48:08 +0200

    changed href

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-orange-400 hover:text-white md:mt-4"
-              href="/about"
+              href="/about/it"
               rel="noreferrer"
             >
               About it
